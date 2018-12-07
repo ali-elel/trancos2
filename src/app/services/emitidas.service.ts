b42b4e5b9b8b426bbc79f9948c6e0d15db584fe1
Luis Mejias <luismejias15@gmail.com>
2018-12-07 11:14:45 -0300

    creado servicio de prueba operations-details-baja.service

---
+++ src/app/services/emitidas.service.ts
@@ -0,0 +1,31 @@
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders,HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+ 
+const httpOptionsUrl = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }), withCredentials: true
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmitidasService {
+  prueba:any;
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getBaja(): Observable<any> {
+    this.prueba=this.http.get('http://jsonplaceholder.typicode.com/users');
+    console.log("En el servicio");
+    return (this.prueba);     
+  }
+  
+
+}
+
+
+ 
