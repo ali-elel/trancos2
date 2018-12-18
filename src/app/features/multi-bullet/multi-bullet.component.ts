c73f674d817c96c96fe4f1fb0c640d7795cd0f7b
Luis Mejias <luismejias15@gmail.com>
2018-12-18 12:19:50 -0300

    commit

--- src/app/features/multi-bullet/multi-bullet.component.ts
+++ src/app/features/multi-bullet/multi-bullet.component.ts
@@ -14,13 +14,13 @@ export class MultiBulletComponent implements OnInit {
   bullet1: GraphBulletOptions;
   bullet2: GraphBulletOptions;
 
-  bullet3 :GraphBulletOptions;
+  bullet3: GraphBulletOptions;
   warranty: Warranty;
   constructor(
     private _WarrantyService: WarrantyService,
 
-  ) { 
-    
+  ) {
+
   }
 
   ngOnInit() {
@@ -155,11 +155,138 @@ export class MultiBulletComponent implements OnInit {
   }
 
   getWarrantyProcess() {
-    this._WarrantyService.getWarranty()
-      .subscribe((res: Warranty) => {
-        this.warranty = res;
-        console.log('this.warranty',this.warranty);
-      });
-  }
+    if (this.type === 'warranty') {
+      console.log(this.type)
+      this._WarrantyService.getWarranty()
+        .subscribe((res: Warranty) => {
+          this.warranty = res;
+          this.bullet1 = {
+            name: 'PESOS',
+            vertical: false,
+            segments: {
+              used: {
+                text: 'Utilizado',
+                value: res.ars.utilizado,
+                currency: 'ARS',
+                backgroundColor: '#5D95FF'
+              },
+              proyected: {
+                text: 'Proyectado',
+                value: res.ars.proyectado,
+                currency: 'ARS',
+                backgroundColor: '#8FB4FF'
+              },
+              available: {
+                text: 'Disponible',
+                value: res.ars.capacidad,
+                currency: 'ARS',
+                backgroundColor: '#E9F0FF'
+              },
+              exceded: {
+                text: 'Excedido',
+                value: res.ars.excedido,
+                currency: 'ARS',
+                backgroundColor: '#0066FF'
+              }
+            }
+          };
 
+          this.bullet2 = {
+            name: 'DÓLARES',
+            vertical: false,
+            segments: {
+              used: {
+                text: 'Utilizado',
+                value: res.usd.utilizado,
+                currency: 'ARS',
+                backgroundColor: '#5D95FF'
+              },
+              proyected: {
+                text: 'Proyectado',
+                value: res.usd.proyectado,
+                currency: 'ARS',
+                backgroundColor: '#8FB4FF'
+              },
+              available: {
+                text: 'Disponible',
+                value: res.usd.capacidad,
+                currency: 'ARS',
+                backgroundColor: '#E9F0FF'
+              },
+              exceded: {
+                text: 'Excedido',
+                value: res.usd.excedido,
+                currency: 'ARS',
+                backgroundColor: '#0066FF'
+              }
+            }
+          };
+
+        });
+    } else if (this.type === 'process') {
+      console.log(this.type)
+      this.bullet1 = {
+        name: 'INTERBANKING',
+        vertical: false,
+        segments: {
+          used: {
+            text: 'Utilizado',
+            value: 1000,
+            currency: 'ARS',
+            backgroundColor: '#00CBD2'
+          },
+          proyected: {
+            text: 'Proyectado',
+            value: 12000,
+            currency: 'ARS',
+            backgroundColor: '#8FB4FF'
+          },
+          available: {
+            text: 'Disponible',
+            value: 20000,
+            currency: 'ARS',
+            backgroundColor: '#E3F8F9'
+          },
+          exceded: {
+            text: 'Excedido',
+            value: 0,
+            currency: 'ARS',
+            backgroundColor: '#00B5BE'
+          }
+        }
+      };
+
+      this.bullet2 = {
+        name: 'BCRA',
+        vertical: false,
+        segments: {
+          used: {
+            text: 'Utilizado',
+            value: 400000,
+            currency: 'ARS',
+            backgroundColor: '#D988A7'
+          },
+          proyected: {
+            text: 'Proyectado',
+            value: 10200,
+            currency: 'ARS',
+            backgroundColor: '#8FB4FF'
+          },
+          available: {
+            text: 'Disponible',
+            value: 1000000,
+            currency: 'ARS',
+            backgroundColor: '#F9ECF0'
+          },
+          exceded: {
+            text: 'Excedido',
+            value: 0,
+            currency: 'ARS',
+            backgroundColor: '#C14978'
+          }
+        }
+      };
+
+    }
+  }
 }
---
+++ src/app/model/warranty.ts
@@ -0,0 +1,10 @@
+export class Warranty {
+    ars: any;
+    usd: any;     
+}
+
+export class Process {
+    bcraTxs: any;
+    interbankingTxs: any;
+}
+
